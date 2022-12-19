89343764b29534089b583f2b08de119fa266a875
dinesh24murali <dinesh24murali@gmail.com>
2022-12-19 20:12:40 +0530

    With example folder

---
+++ example/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import { TestComponent } from 'rollup_react_plugin_example';
+
+function App() {
+  return (
+    <div className="App">
+      <TestComponent />
+    </div>
+  );
+}
+
+export default App;
